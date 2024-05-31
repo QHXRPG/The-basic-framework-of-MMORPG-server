899e65070b84fcf814373f816e16ce10ab666da2
QHXRPG <1342998199@qq.com>
2024-05-31 16:03:54 +0800

    在单例模式的懒汉模式中加入了双检测锁机制

--- Common/Singleton.cs
+++ Common/Singleton.cs
@@ -6,22 +6,30 @@ using System.Threading.Tasks;
 
 namespace Summer
 {
-    //where T: new() 参数类型约束，T必须要有一个无参构造函数
+
     /// <summary>
     /// 单例模式基础类
     /// </summary>
     /// <typeparam name="T">泛型</typeparam>
     public class Singleton<T> where T : new()
     {
-        //问号代表可空类型
         private static T? instance;
+
+        private static object lockObj = new();
+
         public static T Instance
         {
             get
             {
-                if (instance == null)
+                //线程安全的单例对象
+                if (instance != null) 
+                    return instance;
+                lock (lockObj)
                 {
-                    instance = new T();
+                    if (instance == null)
+                    {
+                        instance = new T();
+                    }
                 }
                 return instance;
             }
